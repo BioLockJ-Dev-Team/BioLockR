8ad3e13fa05b9c2116effb867756c9dc43b840dc
Ivory <IvoryEC@gmail.com>
2021-02-19 01:51:41 -0500

    Trivial fix to version test.

--- tests/testthat/test-biolockjVersion.R
+++ tests/testthat/test-biolockjVersion.R
@@ -1,4 +1,4 @@
 test_that("version starts with v", {
     ver = biolockjVersion()
-    expect_true(startsWith(ver, "v") || ver == "")
+    expect_true(startsWith(ver, "BioLockJ v") || ver == "")
 })
